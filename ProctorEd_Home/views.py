aa48a9ae43851bd670cee306a1ebe730fd99c2c0
AP312 <adhish312@gmail.com>
2022-01-20 00:06:10 +0530

    Fourth Commit

---
+++ ProctorEd_Home/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def display_home(request):
+    return render(request, 'Home.html')
+
+
+def display_login(request):
+    return render(request, 'Login.html')
+
+
+def display_cquiz(request):
+    return render(request, 'CreateQuiz.html')
+
+
+def display_quiz(request):
+    return render(request, 'Quiz.html')
+
+
+def display_participate(request):
+    return render(request, 'Participate.html')
+
+
+
